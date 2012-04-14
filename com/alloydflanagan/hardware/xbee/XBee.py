353943427f02c8f9ae78dd502d5f96d4fa2780de
A. L. Flanagan <a.lloyd.flanagan@gmail.com>
2012-04-14 11:43:32 -0400

    =Copyright notice.

--- com/alloydflanagan/hardware/xbee/XBee.py
+++ com/alloydflanagan/hardware/xbee/XBee.py
@@ -1,19 +1,36 @@
 # -*- coding: utf-8 -*-
-from __future__ import division, print_function
-'''
+from __future__ import division, print_function, unicode_literals
+
+#Copyright 2012 A. Lloyd Flanagan
+#This file is part of Pyxb.
+
+#Pyxb is free software: you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation, either version 3 of the License, or
+#(at your option) any later version.
+
+#Pyxb is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with Pyxb.  If not, see <http://www.gnu.org/licenses/>.
+
+"""
 Created on Mar 5, 2012
 
-@author: lloyd
-'''
+@author: A. Lloyd Flanagan
+"""
 
 class XBee(object):
-    '''
+    """
     A single connected XBee radio
-    '''
+    """
 
 
     def __init__(self, device):
-        '''
+        """
         Constructor
-        '''
-        
+        """
+

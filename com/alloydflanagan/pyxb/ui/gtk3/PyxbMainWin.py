ed4b77f51fc3861054ad1db78e67d5b30e50ae84
A. L. Flanagan <a.lloyd.flanagan@gmail.com>
2012-07-08 21:29:53 -0400

    Add pages 2 and 3 to settings display notebook.

--- com/alloydflanagan/pyxb/ui/gtk3/PyxbMainWin.py
+++ com/alloydflanagan/pyxb/ui/gtk3/PyxbMainWin.py
@@ -9,8 +9,9 @@ from gi.repository import Gtk  # @UnresolvedImport uses a dynamic importer.
 from serial.tools import list_ports
 from serial.serialutil import SerialException
 #hmmm.. can't get relative import to work, python says not package?!?!?!
-from com.alloydflanagan.pyxb.ui.gtk3.SettingsNotebook import BasicSettingContents
 #from .SettingsNotebook import BasicSettingContents
+from com.alloydflanagan.pyxb.ui.gtk3.SettingsNotebook import(
+    BasicSettingContents, Network1SettingContents, Network2SettingContents)
 
 
 class PyxbMainWin(object):
@@ -25,14 +26,25 @@ class PyxbMainWin(object):
         self.ports_list = self.builder.get_object("liststore1")
         self.ports_view = self.builder.get_object("dev_list_tview")
         self.page1_child = self.builder.get_object("stg_pg1_child")
-        #self.page2_child =
+        self.stg_notebook = self.builder.get_object("settings_notebook")
+
+        self.page2_child = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+        page2_pos = self.stg_notebook.append_page(self.page2_child,
+                                                  Gtk.Label("Network1"))
+        assert page2_pos == 1
+        self.page2_child.set_property("row-homogeneous", True)
+
+        self.page3_child = Gtk.Grid(orientation=Gtk.Orientation.VERTICAL)
+        page3_pos = self.stg_notebook.append_page(self.page3_child,
+                                                  Gtk.Label("Network2"))
+        assert page3_pos == 2
+        self.page3_child.set_property("row-homogeneous", True)
+
         self.text_view = self.builder.get_object("textview1")
         renderer = Gtk.CellRendererText()
         column = Gtk.TreeViewColumn(None, renderer, text=0)
         column.set_widget(None)  # no header
         self.ports_view.append_column(column)
-
-        self.stg_notebook = self.builder.get_object("settings_notebook")
 
         self.win.connect("delete-event", Gtk.main_quit)
         self.close_btn.connect("clicked", self.close_btn_clicked)
@@ -55,16 +67,24 @@ class PyxbMainWin(object):
             self.selected_port = model[treeiter][0]
             if treeiter != None:
                 print ("You selected", self.selected_port)
-                for child in self.page1_child.get_children():
-                    self.page1_child.remove(child)
                 try:
+                    for child in self.page1_child.get_children():
+                        self.page1_child.remove(child)
                     self.p1_panel = BasicSettingContents(self.selected_port,
                                                          self.page1_child)
+                    for child in self.page2_child.get_children():
+                        self.page2_child.remove(child)
+                    self.p2_panel = Network1SettingContents(self.selected_port,
+                                                            self.page2_child)
+                    for child in self.page3_child.get_children():
+                        self.page3_child.remove(child)
+                    self.p3_panel = Network2SettingContents(self.selected_port,
+                                                            self.page3_child)
                 except SerialException as err:
                     buff = self.text_view.get_buffer()
                     buff.insert_at_cursor("{}: {}\n".format(self.selected_port,
                                                             str(err)))
-                #self.stg_notebook.set_current_page(0)
+            #self.stg_notebook.set_current_page(0)
             self.win.show_all()
 
     def close_btn_clicked(self, event):

56d543b8fa1d977ab1a4388e9250bed89a36b668
A. L. Flanagan <a.lloyd.flanagan@gmail.com>
2013-09-07 12:53:14 -0400

    use ports_chooser to actually fill ports list

--- ui/gtk3/TestSend.py
+++ ui/gtk3/TestSend.py
@@ -11,18 +11,21 @@ if __name__ == "__main__" and __package__ is None:
 
 import os
 import sys 
+
 #we use fakegir to generate package info for editor autocomplete. If it's present in PATH, remove it
 fakegir_path = os.path.join(os.path.expanduser('~'), '.cache', 'fakegir')
 if fakegir_path in sys.path:
     #print("fakegir found; ignoring it")
     sys.path.remove(fakegir_path)
     
+import inspect
 from gi.repository import Gtk, Gdk, GObject
 from serial.tools import list_ports
 from serial.serialutil import SerialException
 import serial
 #TODO: need a common top-level package name (and not pyxbee, it's sort-of taken)
 from hardware.xbee.Settings import ReadException
+from ports_chooser import GtkPortChooser
 
 
 class TestSendMainWin(object):
@@ -30,6 +33,17 @@ class TestSendMainWin(object):
     TIMEOUT=2  #seconds
     "Timeout for serial reads"
     
+    def _print_call(self):
+        "Prints the function name of its caller, with args"
+        #TODO: Make into function annotation
+        #arg to stack() is # of lines of context to include (not well documented)
+        s = inspect.stack(0)
+        print(s[1])
+        caller_name = s[1][3]
+        caller_frame = s[1][0]
+        #print([x for x, y in inspect.getmembers(caller_frame)])
+        print(caller_name + "()")
+        
     def __init__(self, baud_rate, *args, **kwargs):
         self.baud_rate = baud_rate
         self.builder = Gtk.Builder()
@@ -43,11 +57,18 @@ class TestSendMainWin(object):
         "model for device list"
         self.ports_view = self.builder.get_object("dev_list_tview")
         "view for device list"
-        
         self.chkSerial = self.builder.get_object("chkSerial")
-        self.chkUSB = self.builder.get_object("chkUSB")
+        self.chkUSB = self.builder.get_object("chkUSB")        
         isinstance(self.chkSerial, Gtk.CheckButton)
         isinstance(self.chkUSB, Gtk.CheckButton)
+        self.ports_chooser = GtkPortChooser(self.ports_list, 
+                                            self.ports_view, 
+                                            self.chkSerial,
+                                            self.chkUSB,
+                                            self
+                                           )
+
+        self.ports_chooser.updateList()
         #isinstance(self.ports_view, Gtk.TreeView) # hint for editor
         #self.ports_view
         #pprint(dir(self.ports_view))
@@ -71,17 +92,13 @@ class TestSendMainWin(object):
         #connect action handlers to code objects
         handlers = {
             #close button
-            "onCloseAction": Gtk.main_quit,
-            #toggle checkbox on device listing
-            "onDeviceCheckbox": self.populate_devices,
-            #pick a specific device
-            "onSelectDevice": self.onSelectDevice,
-            "on_chkUSB_toggled": self.onChkUSB,
-            "on_chkSerial_toggled": self.onChkSerial,
+            "onCloseAction": self.onCloseAction,
+            "on_chkUSB_toggled": self.ports_chooser.onUSBToggled,
+            "on_chkSerial_toggled": self.ports_chooser.onSerialToggled,
+            "onSelectDevice": self.ports_chooser.onPortChosen,
         }
         self.builder.connect_signals(handlers)
         
-        self.populate_devices()
         isinstance(self.text_view, Gtk.TextView)
         buff = self.text_view.get_buffer()
         buff.insert_at_cursor("{}.{}.{}".format(Gtk.get_major_version(), Gtk.get_minor_version(), Gtk.get_micro_version()))
@@ -89,25 +106,24 @@ class TestSendMainWin(object):
         self.win.show_all()
         
     def onChkUSB(self, button):
+        self._print_call()
         #isinstance(button, Gtk.
-        print(*args)
-        print(**kwargs)
+        print("onChkUSB")
         
-    def onChkSerial(self, *args, **kwargs):
-        print(*args)
-        print(**kwargs)
+    def onChkSerial(self, button):
+        self._print_call()
         
-    def onSelectDevice(self, *args, **kwargs):
-        print(*args)
-        print(**kwargs)
+    def onPortSelected(self, port):
+        print("selected {}".format(port))
+        self.ports_chooser.updateList()
 
-    def populate_devices(self):
-        ports = list_ports.comports()
+    def onCloseAction(self):
+        Gtk.main_quit()
 
-
+        
 if __name__ == '__main__':
     import sys
     print(sys.path)
-    sys.exit(0)
+#    sys.exit(0)
     win = TestSendMainWin(115200)
     Gtk.main()

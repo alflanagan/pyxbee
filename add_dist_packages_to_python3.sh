f837ef594cc263a9a4ca94e627f2db78ed38d60f
alflanagan <a.lloyd.flanagan@gmail.com>
2013-11-30 13:54:53 -0500

    Small script to add required GTK3 packages to python 3 setup. Getting this to work is way too tricky.

---
+++ add_dist_packages_to_python3.sh
@@ -0,0 +1,9 @@
+#!/bin/dash
+DEST_DIR=$(ls -d ${VIRTUAL_ENV}/lib/pyth*/site-packages)
+DIST_PKGS=/usr/lib/python3/dist-packages
+#echo ${DEST_DIR}
+for F in cairo dbus _dbus_bindings.cpython-33m-x86_64-linux-gnu.so _dbus_glib_bindings.cpython-33m-x86_64-linux-gnu.so gi
+do
+	rm ${DEST_DIR}/${F}
+	ln -s ${DIST_PKGS}/${F} ${DEST_DIR}/${F}
+done

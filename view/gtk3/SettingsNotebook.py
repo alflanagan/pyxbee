a6b7d6116bb7e722dfdb56cdbf19fb6ca57226c2
alflanagan <a.lloyd.flanagan@gmail.com>
2014-02-15 16:47:09 -0500

    some cleanup

--- view/gtk3/SettingsNotebook.py
+++ view/gtk3/SettingsNotebook.py
@@ -8,15 +8,14 @@ from xbee import ZigBee
 from serial import Serial
 from model.xbee.config import Settings
 
-
 class SettingContents(object):
     """
     Defines common functionality of classes to "own" contents of
     GridSizer set up to display basic settings of the XBee device.
     """
-    TIMEOUT=2  #seconds
+    TIMEOUT = 2  # seconds
     "Timeout for serial port reads"
-    
+
     def __init__(self, names, labels, entries, baud_rate):
         """
         @param names: Names for the settings displayed (any of the ones defined
@@ -36,8 +35,11 @@ class SettingContents(object):
         """Gtk.Label objects for page, indexed by label's text."""
         self.stg_entries = entries
         """Gtk.Entry objects for page, indexed by label's text."""
+        self.current_port = None
+        self.xb_radio = None
 
     def _set_device(self, device_name):
+        "Binds this object to device, allowing it to display/edit settings for that device."
 # args to serial
 #         port = None,           # number of device, numbering starts at
 #                                # zero. if everything fails, the user
@@ -56,11 +58,12 @@ class SettingContents(object):
 #                                #override if True or False
 #         interCharTimeout=None  # Inter-character timeout, None to disable
         self.current_port = Serial(port=device_name, baudrate=self.baud_rate, timeout=self.TIMEOUT)
-        self.xb = ZigBee(self.current_port, shorthand=True, escaped=False)
-        assert self.xb
-        self.settings.bind(self.xb)
+        self.xb_radio = ZigBee(self.current_port, shorthand=True, escaped=False)
+        assert self.xb_radio
+        self.settings.bind(self.xb_radio)
 
     def populate(self):
+        "Read settings from device and save them."
         curr_vals = self.settings.read_all()
         for stg in self.settings:
             val = curr_vals[stg]
@@ -68,6 +71,7 @@ class SettingContents(object):
 
 
 class NotebookPageSettingContents(SettingContents):
+    "Defines a notebook page which contains a GridSizer set up to display/edit settings."
 
     ATCMDS = ()
     SETTINGS_COUNT = 9  # max settings/page
@@ -86,7 +90,7 @@ class NotebookPageSettingContents(SettingContents):
             grid_sizer_to_populate.attach_next_to(self.stg_entries[lbl],
                                                   self.stg_lbls[lbl],
                                                   Gtk.PositionType.RIGHT, 1, 1)
-        for unused in range(self.SETTINGS_COUNT - len(self.ATCMDS)):
+        for _ in range(self.SETTINGS_COUNT - len(self.ATCMDS)):
             pad1 = Gtk.Label("")
             grid_sizer_to_populate.add(pad1)
             grid_sizer_to_populate.attach_next_to(Gtk.Label(""),
@@ -94,7 +98,7 @@ class NotebookPageSettingContents(SettingContents):
                                                   Gtk.PositionType.RIGHT, 1, 1)
 
         super(NotebookPageSettingContents, self).__init__(self.ATCMDS, self.stg_lbls,
-                                                   self.stg_entries, self.baud_rate, 
+                                                   self.stg_entries, self.baud_rate,
                                                    *args, **kwargs)
         self._set_device(device_name)
         self.populate()
@@ -111,13 +115,11 @@ class BasicSettingContents(NotebookPageSettingContents):
 
 
 class Network1SettingContents(NotebookPageSettingContents):
-    """
-    Class to "own" some settings related to network
-    """
+    "A notebook page which displays settings related to ZigBee networking."
     ATCMDS = ("PAN ID", "Oper Channel", "Oper PAN ID", "Max Uni Hops", "Bcast Hops",
               "Disc T/O", "Disc Opt", "Scan Channels", "Scan Duration",)
 
 
 class Network2SettingContents(NotebookPageSettingContents):
-
+    "Another notebook page to display settings for networking."
     ATCMDS = ("Stack Prof", "Join Time", "Chan Ver", "Net WD TO", "Join Notif", "Aggr Rtg Not")

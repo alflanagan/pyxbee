6b3b806cc83586283a0d56ba739d6b191062264d
A. L. Flanagan <a.lloyd.flanagan@gmail.com>
2014-02-15 13:08:53 -0500

    Initial implementation of some tests.

--- hardware/xbee/test/test_settings.py
+++ hardware/xbee/test/test_settings.py
@@ -4,11 +4,44 @@ Unit tests for the Settings module.
 @author: A. Lloyd Flanagan
 
 '''
+
 import unittest
+import serial
+
+from xbee.base import ReadTimeoutException
+from xbee import ZigBee
+
+from hardware.xbee.config import ReadableSetting, ReadException, WritableSetting, Settings, at_cmds
 
 
 class TestSettings(unittest.TestCase):
-    
+    """Unit tests for hardware.xbee.Settings object."""
+
     @classmethod
     def setUpClass(cls):
-        pass
+        cls.settings = Settings(at_cmds.keys())
+
+    def test_readall(self):
+        port = serial.Serial('/dev/ttyUSB0', 38400)
+        xbee_device = ZigBee(port)
+        self.settings.bind(xbee_device)
+        read_settings = self.settings.read_all()
+        #TODO: clearly we need to allow for other versions, etc.
+        self.assertEqual(read_settings['Version'], '21A7 Coord(API)')
+        #TODO: and this needs to be configurable
+        self.assertEqual(read_settings['PAN ID'], '000000000BADFACE')
+        xbee_device.halt()
+        port.close()
+
+    def test_read_timeout(self):
+        port = serial.Serial('/dev/ttyUSB0', 9600)
+        xbee_device = ZigBee(port)
+        self.settings.bind(xbee_device)
+        self.assertRaises(ReadException,
+                          self.settings.read_all)
+        xbee_device.halt()
+        port.close()
+
+
+if __name__ == '__main__':
+    unittest.main()

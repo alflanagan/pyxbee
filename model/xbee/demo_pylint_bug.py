6d2a689648d9e64f39b28db1e65f73d85673f12b
alflanagan <a.lloyd.flanagan@gmail.com>
2014-04-20 20:43:42 -0400

    Deleting as bug no longer seen.

--- model/xbee/demo_pylint_bug.py
+++
@@ -1,7 +0,0 @@
-from collections import MutableMapping
-
-class CustomMapping(MutableMapping):
-
-    def __init__(self, *args, **kwargs):
-        #triggers E1002, which should *never* occur in python 3 (where old-style classes don't exist)
-        super().__init__(*args, **kwargs)
